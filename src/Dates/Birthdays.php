d8cd6a891e944fca53238150f625ed27cdc2d4c1
Nikita <nikita.omega2000@outlook.com>
2019-03-29 09:25:56 +0000

    Refactored to class

--- src/Dates/Birthdays.php
+++ src/Dates/Birthdays.php
@@ -11,5 +11,43 @@ namespace SweetBrides\Dates;
 
 class Birthdays
 {
+    private $birthDate;
+    private $todayDate;
+
+    public function __construct($birthDate)
+    {
+        $this->birthDate = $birthDate;
+        $this->todayDate = date("Y-m-d");
+    }
+
+    public function findTotalDays()
+    {
+        $birthTime = strtotime($this->birthDate);
+        $todayTime = strtotime($this->todayDate);
+
+        $totalSecs = $todayTime - $birthTime;
+        $totalHours = floor($totalSecs / 3600);
+        $totalDays = floor($totalHours / 24);
+        return $totalDays;
+    }
+
+    public function findTotalHours()
+    {
+        $birthTime = strtotime($this->birthDate);
+        $todayTime = strtotime($this->todayDate);
+
+        $totalSecs = $todayTime - $birthTime;
+        $totalHours = floor($totalSecs / 3600);
+        return $totalHours;
+    }
+
+    public function findTotalSecs()
+    {
+        $birthTime = strtotime($this->birthDate);
+        $todayTime = strtotime($this->todayDate);
+
+        $totalSecs = $todayTime - $birthTime;
+        return $totalSecs;
+    }
 
 }
